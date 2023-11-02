3b43c3cb162fe331528548be82b61e4b8e1a58b9
evi1Husky <evi1husky0@gmail.com>
2023-11-02 16:50:40 +1000

    add updates handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { NoiseGenerator } from "./components/NoiseGenerator/NoiseGenerator";
+import { registerSW } from "virtual:pwa-register";
 
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <div className="app-container">
